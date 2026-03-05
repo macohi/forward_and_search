ee3505138ba1ca4b0dd3c70328181db8942836ea
Macohi <sinconsistencia@gmail.com>
2026-03-05 16:20:04 -0600

    feat: scene data (nothing yet) and scene manager

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,19 @@
 package;
 
+import backend.Scene;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var scene:SceneData = {};
+
+	override public function new(scene:String)
+	{
+		super();
+
+		this.scene = SceneManager.getScene(scene);
+	}
+
 	override public function create()
 	{
 		super.create();
---
+++ source/backend/Scene.hx
@@ -0,0 +1,26 @@
+package backend;
+
+import lime.utils.Assets;
+import haxe.Json;
+import lime.app.Application;
+
+typedef SceneData = {}
+
+class SceneManager
+{
+	public static function getScene(scene:String):SceneData
+	{
+		var sceneData:SceneData = {};
+
+		try
+		{
+			sceneData = Json.parse(Assets.getText('assets/data/scenes/$scene.json'));
+		}
+		catch (e)
+		{
+			Application.current.window.alert('Error parsing scene: ${scene}\n\n${e}', 'SCENEMANAGER : PARSING ERROR');
+		}
+
+		return sceneData;
+	}
+}
